849db8c39daa5e0102454581ae710749e65b7614
tang-pc\tang <768013173@qq.com>
2020-09-04 14:58:33 +0800

    修改支持自定义头部从外部传入

---
+++ coordinatortablayout/src/main/java/cn/hugeterry/coordinatortablayout/CoordinatorTabLayoutWithHead.java
@@ -0,0 +1,276 @@
+package cn.hugeterry.coordinatortablayout;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import cn.hugeterry.coordinatortablayout.listener.LoadHeaderImagesListener;
+import cn.hugeterry.coordinatortablayout.listener.OnTabSelectedListener;
+import cn.hugeterry.coordinatortablayout.utils.SystemView;
+
+/**
+ * @author hugeterry(http://hugeterry.cn)
+ */
+
+public class CoordinatorTabLayoutWithHead extends CoordinatorLayout {
+
+    private Context mContext;
+    private Toolbar mToolbar;
+    private ActionBar mActionbar;
+    private TabLayout mTabLayout;
+    private FrameLayout mHeadView;
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private LoadHeaderImagesListener mLoadHeaderImagesListener;
+    private OnTabSelectedListener mOnTabSelectedListener;
+
+    public CoordinatorTabLayoutWithHead(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    public CoordinatorTabLayoutWithHead(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        if (!isInEditMode()) {
+            initView(context);
+            initWidget(context, attrs);
+        }
+    }
+
+    public CoordinatorTabLayoutWithHead(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+        if (!isInEditMode()) {
+            initView(context);
+            initWidget(context, attrs);
+        }
+    }
+
+    private void initView(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.view_coordinatortablayout_head, this, true);
+        initToolbar();
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingtoolbarlayout);
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mHeadView = (FrameLayout) findViewById(R.id.headView);
+    }
+
+    private void initWidget(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs
+                , R.styleable.CoordinatorTabLayout);
+
+        TypedValue typedValue = new TypedValue();
+        mContext.getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        int contentScrimColor = typedArray.getColor(
+                R.styleable.CoordinatorTabLayout_contentScrim, typedValue.data);
+        mCollapsingToolbarLayout.setContentScrimColor(contentScrimColor);
+
+        int tabIndicatorColor = typedArray.getColor(R.styleable.CoordinatorTabLayout_tabIndicatorColor, Color.WHITE);
+        mTabLayout.setSelectedTabIndicatorColor(tabIndicatorColor);
+
+        int tabTextColor = typedArray.getColor(R.styleable.CoordinatorTabLayout_tabTextColor, Color.WHITE);
+        mTabLayout.setTabTextColors(ColorStateList.valueOf(tabTextColor));
+        typedArray.recycle();
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        ((AppCompatActivity) mContext).setSupportActionBar(mToolbar);
+        mActionbar = ((AppCompatActivity) mContext).getSupportActionBar();
+    }
+
+    /**
+     * 设置Toolbar标题
+     *
+     * @param title 标题
+     * @return CoordinatorTabLayout
+     */
+    public CoordinatorTabLayoutWithHead setTitle(String title) {
+        if (mActionbar != null) {
+            mActionbar.setTitle(title);
+        }
+        return this;
+    }
+
+    /**
+     * 设置Toolbar显示返回按钮及标题
+     *
+     * @param canBack 是否返回
+     * @return CoordinatorTabLayout
+     */
+    public CoordinatorTabLayoutWithHead setBackEnable(Boolean canBack) {
+        if (canBack && mActionbar != null) {
+            mActionbar.setDisplayHomeAsUpEnabled(true);
+            mActionbar.setHomeAsUpIndicator(R.drawable.ic_arrow_white_24dp);
+        }
+        return this;
+    }
+    private void setupTabLayout() {
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (mOnTabSelectedListener != null) {
+                    mOnTabSelectedListener.onTabSelected(tab);
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                if (mOnTabSelectedListener != null) {
+                    mOnTabSelectedListener.onTabUnselected(tab);
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                if (mOnTabSelectedListener != null) {
+                    mOnTabSelectedListener.onTabReselected(tab);
+                }
+            }
+        });
+    }
+
+    /**
+     * 设置TabLayout TabMode
+     *
+     * @param mode
+     * @return CoordinatorTabLayout
+     */
+    public CoordinatorTabLayoutWithHead setTabMode(@TabLayout.Mode int mode) {
+        mTabLayout.setTabMode(mode);
+        return this;
+    }
+
+    /**
+     * 设置与该组件搭配的ViewPager
+     *
+     * @param viewPager 与TabLayout结合的ViewPager
+     * @return CoordinatorTabLayout
+     */
+    public CoordinatorTabLayoutWithHead setupWithViewPager(ViewPager viewPager) {
+        setupTabLayout();
+        mTabLayout.setupWithViewPager(viewPager);
+        return this;
+    }
+
+    /**
+     * 获取该组件中的ActionBar
+     */
+    public ActionBar getActionBar() {
+        return mActionbar;
+    }
+
+    /**
+     * 获取该组件中的TabLayout
+     */
+    public TabLayout getTabLayout() {
+        return mTabLayout;
+    }
+
+    /**
+     * 获取该组件中的ImageView
+     */
+    public FrameLayout getImageView() {
+        return mHeadView;
+    }
+
+    /**
+     * 设置LoadHeaderImagesListener
+     *
+     * @param loadHeaderImagesListener 设置LoadHeaderImagesListener
+     * @return CoordinatorTabLayout
+     */
+    public CoordinatorTabLayoutWithHead setLoadHeaderImagesListener(LoadHeaderImagesListener loadHeaderImagesListener) {
+        mLoadHeaderImagesListener = loadHeaderImagesListener;
+        return this;
+    }
+
+    /**
+     * 设置onTabSelectedListener
+     *
+     * @param onTabSelectedListener 设置onTabSelectedListener
+     * @return CoordinatorTabLayout
+     */
+    public CoordinatorTabLayoutWithHead addOnTabSelectedListener(OnTabSelectedListener onTabSelectedListener) {
+        mOnTabSelectedListener = onTabSelectedListener;
+        return this;
+    }
+
+    /**
+     * 设置透明状态栏
+     *
+     * @param activity 当前展示的activity
+     * @return CoordinatorTabLayout
+     */
+    public CoordinatorTabLayoutWithHead setTranslucentStatusBar(@NonNull Activity activity) {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return this;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+            activity.getWindow()
+                    .getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            activity.getWindow()
+                    .setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        if (mToolbar != null) {
+            MarginLayoutParams layoutParams = (MarginLayoutParams) mToolbar.getLayoutParams();
+            layoutParams.setMargins(
+                    layoutParams.leftMargin,
+                    layoutParams.topMargin + SystemView.getStatusBarHeight(activity),
+                    layoutParams.rightMargin,
+                    layoutParams.bottomMargin);
+        }
+
+        return this;
+    }
+
+    /**
+     * 设置沉浸式
+     *
+     * @param activity 当前展示的activity
+     * @return CoordinatorTabLayout
+     */
+    public CoordinatorTabLayoutWithHead setTranslucentNavigationBar(@NonNull Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return this;
+        } else {
+            mToolbar.setPadding(0, SystemView.getStatusBarHeight(activity) >> 1, 0, 0);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        return this;
+    }
+
+}
